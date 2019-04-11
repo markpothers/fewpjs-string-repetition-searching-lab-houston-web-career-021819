cf44573e1024a4a436d36829b1a02b3cab94bab2
Mark R Pothecary <markpothers@hotmail.com>
2019-04-11 16:11:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+
+function heyJude(){
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(string){
+    return string.search(/candycorn|brusselssprouts/)
+}
